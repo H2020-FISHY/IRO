143827c1a53be3d06f200db9895e636f72bd535a
Marc Michalke <m.michalke@tu-braunschweig.de>
2021-12-01 16:48:30 +0100

    Added IP config via env variable

--- iro/entrypoint.sh
+++ iro/entrypoint.sh
@@ -1,14 +1,29 @@
 #!/bin/bash -e
-#IFACES="eth0 eth1"
-for i in $INTERFACES_UP
+#expecting interfaces as envronment variable following patter:
+#INTERFACES_UP="eth0:123.123.123.123/24 eth1:124.124.124.3/26"
+INTERFACE_DEFS=($INTERFACES_UP)
+echo "Configuring IP addresses, expecting config in environment variable like this: INTERFACES_UP=\"eth0:123.123.123.123/24 eth1:124.124.124.3/26\""
+for i in "${!INTERFACE_DEFS[@]}"
 do
-echo "bringing up interface $i..."
-if ip link set $i up; then
-echo "brought $i up successfully"
-else
-echo "unable to bring up interface $i"
-fi
+	IF=$(cut -d : -f 1 <<< ${INTERFACE_DEFS[$i]})
+	IP=$(cut -d : -f 2 <<< ${INTERFACE_DEFS[$i]})
+	if $(ip address show $IF | grep $IP) != 0; then
+		echo "setting $IF address to $IP..."
+		if ip addr add $IP dev $IF; then
+			echo "IP $IP for interface $IF set successfully"
+			echo "bringing up interface $IF..."
+			else
+			echo "unable to set IP address for interface $IF"
+		fi
+	else
+		echo "address $IP for interface $IF is already set"
+	fi
+	if ip link set $IF up; then
+		echo "brought $IF up successfully"
+		else
+		echo "unable to bring up interface $IF"
+	fi
 done
 echo "Interfaces:"
-ip link sh
+ip addr sh
 python /iro/main.py
