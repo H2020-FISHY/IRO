b66a8a2d57a4c4e5cc3e9de1e8b00b636fe7d2c4
Marc M <marc@michalke-it.de>
2022-03-21 15:22:55 +0000

    Feature: added HTTPS support

--- iro/main.py
+++ iro/main.py
@@ -130,4 +130,10 @@ class UserInterface(FlaskView):
 
 if __name__ == "__main__":
     UserInterface.register(app, route_base='/')
-    app.run(host='0.0.0.0', port=5000, debug=True)
+    certs_present = [os.path.exists('/crt/cert.pem'),
+                    os.path.exists('/crt/key.pem')]
+    print("Detected [certificate, private key]: ", certs_present)
+    if all(certs_present):
+        app.run(host='0.0.0.0', port=5000, debug=True, ssl_context=('/crt/cert.pem', '/crt/key.pem'))
+    else:
+        app.run(host='0.0.0.0', port=5000, debug=True)
