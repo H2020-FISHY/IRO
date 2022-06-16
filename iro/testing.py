af0c7e05b7083068b5b90de36306b61d9c28630d
mounir-bs <mounirsalem123@gmail.com>
2022-06-16 19:02:40 +0200

    cleaning

--- iro/testing.py
+++
@@ -1,7 +0,0 @@
-import xml.etree.ElementTree as ET
-root = ET.parse('policies/hspl1.xml').getroot()
-print(root)
-for child in root:
-    value = child.tag
-    print(value, child.attrib)
-print([elem.tag for elem in root.iter()])
